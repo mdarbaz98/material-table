530042de9237ef3efff7e05d997c6569dda64227
mdarbaz98 <zainshaikh2468@gmail.com>
2021-09-28 15:45:12 +0530

    first commit for material-table

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Table from "./Table";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React MaterialTable</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
